c7c6284450533d84715396f9ddaf26c6dac51f02
Jirateep <70580937+Jirateep12@users.noreply.github.com>
2021-05-26 20:59:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,25 +17,30 @@
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">First name:</label>
                 <input type="text" name="firstname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your name with anyone else.</div>
+                <div id="emailHelp" class="form-text">We'll never share your full name with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Last name:</label>
                 <input type="text" name="lastname" class="form-control" id="exampleInputPassword1">
+                <div id="emailHelp" class="form-text">We'll never share your last name with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Email:</label>
                 <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Phone:</label>
-                <input type="number" name="phone" class="form-control" id="exampleInputPassword1">
+                <input type="number" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==10) return false;" name="phone" class="form-control" id="exampleInputPassword1">
+                <div id="emailHelp" class="form-text">We'll never share your phone with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">More:</label>
                 <textarea class="form-control" name="more" id="" cols="30" rows="10"></textarea>
             </div>
-            <button type="submit" name="submit" class="btn btn-success">Submit</button>
+            <div class="d-grid">
+                <button type="submit" name="submit" class="btn btn-outline-success">Submit</button>
+            </div>
         </form>
     </div>
 
